bc134ce5e5dab2ba09dd142f0390e8e57f0d2ede
LuMin <lumin.vis@gmail.com>
2019-02-05 11:23:23 +0800

    Add files via upload

--- tornado.server/url.py
+++ tornado.server/url.py
@@ -9,13 +9,17 @@ import sys
 from handler.pointhandler import IndexHandler
 from handler.pointhandler import TestHandler
 from handler.pointhandler import ImgHandler
+from handler.pointhandler import LabeledImgHandler
 from handler.pointhandler import SaveHandler
+from handler.pointhandler import OpenHandler
 from handler.pointhandler import GetHandler
 
 url=[
 	(r'/', IndexHandler),
     # (r'/test/(\w+)', TestHandler),
     (r'/getImgList', ImgHandler),
+    (r'/getLabeledImgList', LabeledImgHandler),
     (r'/save', SaveHandler),
+    (r'/open', OpenHandler),
     (r'/get', GetHandler),
 ]
